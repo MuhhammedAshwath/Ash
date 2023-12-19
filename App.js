4a4d8d963713640a0b266e7a1447605781ecde78
MuhhammedAshwath <153721038+MuhhammedAshwath@users.noreply.github.com>
2023-12-19 10:46:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import MyListComponent from './MyListComponent';
+// import Counter from './Counter'
+// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import Login from './Login';
+// import Signup from './Signup';
+// import Loginadmin from './Loginadmin';
+
+function App() {
+  return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/Login" element={<Login/>} />
+    //     <Route path="/Signup" element={<Signup/>} />
+    //     <Route path="/Loginadmin" element={<Loginadmin/>} />
+    //   </Routes>
+    // </Router>
+    <MyListComponent />
+  );
+};
+// 
+// import React from 'react';
+// import { Button, Typography, Container } from '@mui/material';
+
+// const App = () => {
+//   return (
+//     <Container>
+//       <Typography variant="h2" gutterBottom>
+//         Welcome to My Material-UI App
+//       </Typography>
+//       <Button variant="contained" color="primary">
+//         Click Me
+//       </Button>
+//     </Container>
+//   );
+// };
+
+export default App;
+
